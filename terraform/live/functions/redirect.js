8001db8d6fb68cb3c72b0b0785f39b5ab059861e
makemesteaks <castrofjoao@gmail.com>
2022-10-10 12:17:11 +0100

    fix: rewrite regex

--- terraform/live/functions/redirect.js
+++ terraform/live/functions/redirect.js
@@ -1,5 +1,5 @@
 function handler(event) {
     var request = event.request;
-    if (request.uri.match(/\/starknet.*/)) { request.uri = request.uri.replace(/\/starknet.*/,'/starknet/index.html'); }
+    if (request.uri.match(/\/starknet[a-z]*[0-9]{0,9}[\/]{0,1}$/)) { request.uri = request.uri.replace(/\/starknet.*/,'/starknet/index.html'); }
     return request;
 }
