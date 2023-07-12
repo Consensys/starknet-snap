51de94ac23e01ca6fbbe1414bed7d38bce810cff
stanleyyuen <stanley.yuen@consensys.net>
2023-07-12 15:22:00 +0800

    fix: fix add erc20 token dialog
    - change add erc20 token dialog to use copyable text

--- packages/starknet-snap/src/utils/snapUtils.ts
+++ packages/starknet-snap/src/utils/snapUtils.ts
@@ -197,16 +197,6 @@ export function getSigningTxnText(
   );
 }
 
-export function getAddTokenText(
-  tokenAddress: string,
-  tokenName: string,
-  tokenSymbol: string,
-  tokenDecimals: number,
-  network: Network,
-) {
-  return `Token Address: ${tokenAddress}\n\nToken Name: ${tokenName}\n\nToken Symbol: ${tokenSymbol}\n\nToken Decimals: ${tokenDecimals}\n\nNetwork: ${network.name}`;
-}
-
 export function getAccount(state: SnapState, accountAddress: string, chainId: string) {
   const bigIntAccountAddress = num.toBigInt(accountAddress);
   return state.accContracts?.find(
