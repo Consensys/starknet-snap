88cdaddbbb58ba7303b76666f6ecaedc5aef983a
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-08-23 19:41:30 +0800

    feat: add t and c at footer
    - add t and c
    - add copy

--- packages/wallet-ui/src/components/ui/organism/Footer/Footer.view.tsx
+++ packages/wallet-ui/src/components/ui/organism/Footer/Footer.view.tsx
@@ -1,10 +1,24 @@
-import { MetamaskSnaps, PoweredBy, Wrapper } from './Footer.style';
+import { MetamaskSnaps, PoweredBy, Wrapper, TandCWrapper, TandCLink, CopyText } from './Footer.style';
 
 export const FooterView = () => {
+  const currentYr = new Date().getFullYear();
   return (
-    <Wrapper>
-      <PoweredBy>Powered by </PoweredBy>
-      <MetamaskSnaps>MetaMask Snaps</MetamaskSnaps>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <PoweredBy>Powered by </PoweredBy>
+        <MetamaskSnaps>MetaMask Snaps</MetamaskSnaps>
+
+        <TandCWrapper>
+        <CopyText>&copy;{currentYr} consensys</CopyText>
+        <TandCLink href="https://consensys.io/terms-of-use" target="_blank">
+          Terms of Use
+        </TandCLink>
+        <TandCLink href="https://consensys.io/privacy-policy" target="_blank">
+          Privacy Policy
+        </TandCLink>
+      </TandCWrapper>
+      </Wrapper>
+      
+    </>
   );
 };
