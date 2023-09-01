f33de1512155d7d265d32a5f2ac8b95a88a41575
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-09-01 17:18:02 +0800

    feat: add t and c at footer (#120)
    * feat: add t and c at footer

--- packages/wallet-ui/src/components/ui/organism/Footer/Footer.style.ts
+++ packages/wallet-ui/src/components/ui/organism/Footer/Footer.style.ts
@@ -5,6 +5,7 @@ export const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   height: 120px;
+  flex-wrap: wrap;
 `;
 
 export const PoweredBy = styled.div`
@@ -16,4 +17,24 @@ export const MetamaskSnaps = styled.div`
   font-size: ${(props) => props.theme.typography.p1.fontSize};
   font-weight: ${(props) => props.theme.typography.bold.fontWeight};
   font-family: ${(props) => props.theme.typography.bold.fontFamily};
+`;
+
+export const TandCWrapper = styled.div`
+  flex-basis: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: -48px;
+`;
+
+export const CopyText = styled.div`
+  font-size: ${(props) => props.theme.typography.p2.fontSize};
+`;
+
+export const TandCLink = styled.a`
+  font-size: ${(props) => props.theme.typography.p1.fontSize};
+  font-weight: ${(props) => props.theme.typography.bold.fontWeight};
+  font-family: ${(props) => props.theme.typography.bold.fontFamily};
+  margin-left: ${(props) => props.theme.spacing.tiny2};
+  color: ${(props) => props.theme.palette.grey.black};
 `;
