52baa2de33038d2b6c11f72dc5367b0f769e4608
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-10-12 12:57:27 +0800

    fix: remove consensys.net

--- terraform/live/main.tf
+++ terraform/live/main.tf
@@ -3,25 +3,11 @@
 #############
 
 locals {
-  hosted_zone_name                = "starknet-snap.consensys-solutions.net"
-  hosted_zone_id                  = aws_route53_zone.main.zone_id
-  dev_domain_name                 = "app-dev.${local.hosted_zone_name}"
-  dev_domain_name_alternative     = "dev.snaps.consensys.io"
-  staging_domain_name             = "app-staging.${local.hosted_zone_name}"
-  staging_domain_name_alternative = "staging.snaps.consensys.io"
-  prod_domain_name                = "app.${local.hosted_zone_name}"
-  prod_domain_name_alternative    = "snaps.consensys.io"
-
   # snaps 
-  snaps_hosted_zone_name        = "snaps.consensys.net"
   snaps_hosted_zone_name_new    = "snaps.consensys.io"
-  snaps_hosted_zone_id          = aws_route53_zone.snaps.zone_id
-  dev_snaps_domain_name         = "dev.${local.snaps_hosted_zone_name}"
   dev_snaps_domain_name_new     = "dev.${local.snaps_hosted_zone_name_new}"
   snaps_cert_new                = "arn:aws:acm:us-east-1:905502874957:certificate/9a317fa9-baef-47b8-8814-f387ae646afc"
-  staging_snaps_domain_name     = "staging.${local.snaps_hosted_zone_name}"
   staging_snaps_domain_name_new = "staging.${local.snaps_hosted_zone_name_new}"
-  prod_snaps_domain_name        = local.snaps_hosted_zone_name
   prod_snaps_domain_name_new    = local.snaps_hosted_zone_name_new
 
   #cloudfront functions
@@ -35,57 +21,6 @@ locals {
       event_type = "viewer-response"
     }
   }
-
-  cloudfront_functions_for_deprecated_domain = {
-    redirect = {
-      arn        = aws_cloudfront_function.deprecated_domain_redirect.arn
-      event_type = "viewer-request"
-    }
-  }
-}
-
-resource "aws_route53_zone" "main" {
-  name = local.hosted_zone_name
-  tags = module.tags.common
-}
-
-resource "aws_route53_zone" "snaps" {
-  name = local.snaps_hosted_zone_name
-  tags = module.tags.common
-}
-
-#############
-## Certificate
-#############
-
-module "cert" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "3.5.0"
-
-  providers = {
-    aws = aws.use1
-  }
-
-  subject_alternative_names = ["*.starknet-snap.consensys-solutions.net"]
-  wait_for_validation       = true
-  domain_name               = local.hosted_zone_name
-  zone_id                   = local.hosted_zone_id
-  tags                      = module.tags.common
-}
-
-module "snaps_cert" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "3.5.0"
-
-  providers = {
-    aws = aws.use1
-  }
-
-  subject_alternative_names = ["*.${local.snaps_hosted_zone_name}"]
-  wait_for_validation       = true
-  domain_name               = local.snaps_hosted_zone_name
-  zone_id                   = local.snaps_hosted_zone_id
-  tags                      = module.tags.common
 }
 
 #############
@@ -100,14 +35,6 @@ resource "aws_cloudfront_function" "starknet_redirect" {
   code    = file("${path.module}/functions/redirect-mm.js")
 }
 
-resource "aws_cloudfront_function" "deprecated_domain_redirect" {
-  name    = "starknet-snap-deprecated-domain-redirect"
-  runtime = "cloudfront-js-1.0"
-  comment = "deprecated-domain-redirect"
-  publish = true
-  code    = file("${path.module}/functions/redirect.js")
-}
-
 resource "aws_cloudfront_function" "starknet_add_header" {
   name    = "starknet-snap-add-header"
   runtime = "cloudfront-js-1.0"
@@ -120,34 +47,6 @@ resource "aws_cloudfront_function" "starknet_add_header" {
 #############
 ## Dev
 #############
-
-# module "s3_dev" {
-#   source = "../modules/aws-s3-website"
-
-#   bucket_name     = local.dev_domain_name
-#   domain_name     = local.dev_domain_name
-#   certificate_arn = module.cert.acm_certificate_arn
-#   cloudfront_functions = {
-#     headers = {
-#       arn        = aws_cloudfront_function.starknet_add_header.arn
-#       event_type = "viewer-response"
-#     }
-#   }
-#   hosted_zone_id = local.hosted_zone_id
-#   tags           = module.tags.common
-# }
-
-module "s3_snaps_page_dev" {
-  source = "../modules/aws-s3-website"
-
-  bucket_name          = local.dev_snaps_domain_name
-  domain_name          = local.dev_snaps_domain_name
-  certificate_arn      = module.snaps_cert.acm_certificate_arn
-  hosted_zone_id       = local.snaps_hosted_zone_id
-  cloudfront_functions = local.cloudfront_functions_for_deprecated_domain
-  tags                 = module.tags.common
-}
-
 module "s3_snaps_page_dev_new" {
   source = "../modules/aws-s3-website-no-r53"
 
@@ -161,34 +60,6 @@ module "s3_snaps_page_dev_new" {
 #############
 ## Staging
 #############
-
-# module "s3_staging" {
-#   source = "../modules/aws-s3-website"
-
-#   bucket_name     = local.staging_domain_name
-#   domain_name     = local.staging_domain_name
-#   certificate_arn = module.cert.acm_certificate_arn
-#   hosted_zone_id  = local.hosted_zone_id
-#   cloudfront_functions = {
-#     headers = {
-#       arn        = aws_cloudfront_function.starknet_add_header.arn
-#       event_type = "viewer-response"
-#     }
-#   }
-#   tags = module.tags.common
-# }
-
-module "s3_snaps_page_staging" {
-  source = "../modules/aws-s3-website"
-
-  bucket_name          = local.staging_snaps_domain_name
-  domain_name          = local.staging_snaps_domain_name
-  certificate_arn      = module.snaps_cert.acm_certificate_arn
-  hosted_zone_id       = local.snaps_hosted_zone_id
-  cloudfront_functions = local.cloudfront_functions_for_deprecated_domain
-  tags                 = module.tags.common
-}
-
 module "s3_snaps_page_staging_new" {
   source = "../modules/aws-s3-website-no-r53"
 
@@ -201,34 +72,6 @@ module "s3_snaps_page_staging_new" {
 #############
 ## Prod
 #############
-
-# module "s3_prod" {
-#   source = "../modules/aws-s3-website"
-
-#   bucket_name     = local.prod_domain_name
-#   domain_name     = local.prod_domain_name
-#   certificate_arn = module.cert.acm_certificate_arn
-#   hosted_zone_id  = local.hosted_zone_id
-#   cloudfront_functions = {
-#     headers = {
-#       arn        = aws_cloudfront_function.starknet_add_header.arn
-#       event_type = "viewer-response"
-#     }
-#   }
-#   tags = module.tags.common
-# }
-
-module "s3_snaps_page_prod" {
-  source = "../modules/aws-s3-website"
-
-  bucket_name          = local.prod_snaps_domain_name
-  domain_name          = local.prod_snaps_domain_name
-  certificate_arn      = module.snaps_cert.acm_certificate_arn
-  hosted_zone_id       = local.snaps_hosted_zone_id
-  cloudfront_functions = local.cloudfront_functions_for_deprecated_domain
-  tags                 = module.tags.common
-}
-
 module "s3_snaps_page_prod_new" {
   source = "../modules/aws-s3-website-no-r53"
 
--- terraform/modules/aws-s3-website/output.tf
+++
@@ -1,7 +0,0 @@
-output "cf_domain_name" {
-  value = aws_cloudfront_distribution.dist.domain_name
-}
-
-output "bucket_regional_domain_name" {
-  value = aws_s3_bucket.bucket.bucket_regional_domain_name
-}
