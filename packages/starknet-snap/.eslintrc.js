718b85a44fab1c7157b6a730a159f03bd5d7377e
wantedsystem <amine.harty@consensys.net>
2023-11-07 11:03:25 +0100

    fix: fix lint

--- packages/starknet-snap/.eslintrc.js
+++ packages/starknet-snap/.eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'max-warnings': [2, { count: 100 }],
   },
 };
