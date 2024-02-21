60f37da767504382a4a93a5ce944e6119bd6e304
Iris <iris.devillars@gmail.com>
2024-02-21 06:47:07 +0100

    feat: display Stark name in Sidebar (#184)
    * feat: display Stark name in sidebar
    
    * feat: display name in AccountDetails
    
    * fix: only show in Sidebar
    
    * fix: result schema type for starkNet_getStarkName
    
    * fix: starkNet_getStarkName description
    
    * test: add getStarkName test

--- packages/starknet-snap/src/index.ts
+++ packages/starknet-snap/src/index.ts
@@ -40,6 +40,7 @@ import { declareContract } from './declareContract';
 import { signDeclareTransaction } from './signDeclareTransaction';
 import { signDeployAccountTransaction } from './signDeployAccountTransaction';
 import { logger } from './utils/logger';
+import { getStarkName } from './getStarkName';
 
 declare const snap;
 const saveMutex = new Mutex();
@@ -199,6 +200,9 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => 
     case 'starkNet_declareContract':
       apiParams.keyDeriver = await getAddressKeyDeriver(snap);
       return declareContract(apiParams);
+
+    case 'starkNet_getStarkName':
+      return getStarkName(apiParams);
 
     default:
       throw new Error('Method not found.');
