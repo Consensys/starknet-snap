3c8ccf3179e51fa3088e56911a0418661494c4ac
Florin Dzeladini <florin.dzeladini@consensys.net>
2024-05-24 08:34:08 +0200

    chore: lint and optional env variable for SNAP_ID

--- packages/get-starknet/webpack.config.dev.js
+++ packages/get-starknet/webpack.config.dev.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { merge } = require('webpack-merge');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const common = require('./webpack.common.js');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 module.exports = merge(common, {
   mode: 'development',
