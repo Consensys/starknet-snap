138edc9155f39fa4c2983fc146eb0f0985c7762e
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-06-20 12:02:01 +0800

    chore: update get starknet build config

--- packages/get-starknet/webpack.config.local.js
+++ packages/get-starknet/webpack.config.local.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const Dotenv = require('dotenv-webpack');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpack = require('webpack');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { merge } = require('webpack-merge');
