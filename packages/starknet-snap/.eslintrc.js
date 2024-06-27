7fb62f7038064c7e32a4e005014f02037aa4ac84
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-06-27 18:29:43 +0800

    chore: update config

--- packages/starknet-snap/.eslintrc.js
+++ packages/starknet-snap/.eslintrc.js
@@ -1,23 +1,39 @@
 module.exports = {
-  extends: ['../../.eslintrc.js'],
-
   parserOptions: {
+    sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
 
+  extends: ['@metamask/eslint-config'],
+
   overrides: [
+    {
+      files: ['*.js'],
+      extends: ['@metamask/eslint-config-nodejs'],
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['@metamask/eslint-config-typescript'],
+      rules: {
+        'valid-jsdoc': 'off',
+        'jsdoc/require-description': 'off',
+        'jsdoc/require-returns': 'off',
+        'jsdoc/require-param-description': 'off',
+        'jsdoc/match-description': 'off',
+      },
+    },
+    {
+      files: ['*.test.ts', '*.test.js'],
+      extends: ['@metamask/eslint-config-jest'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error', { allow: ['describe', 'expect', 'it'] }],
+        '@typescript-eslint/unbound-method': 'off',
+      },
+    },
     {
       files: ['snap.config.ts'],
       extends: ['@metamask/eslint-config-nodejs'],
     },
-
-    {
-      files: ['*.test.ts'],
-      rules: {
-        '@typescript-eslint/unbound-method': 'off',
-      },
-    },
-
     {
       files: ['*.ts'],
       rules: {
@@ -25,6 +41,14 @@ module.exports = {
       },
     },
   ],
-
-  ignorePatterns: ['!.eslintrc.js', 'dist/'],
+  ignorePatterns: [
+    '!.prettierrc.js',
+    '**/!.eslintrc.js',
+    '**/dist*/',
+    '**/*__GENERATED__*',
+    '**/build',
+    '**/public',
+    '**/.cache',
+    '**/test',
+  ],
 };
