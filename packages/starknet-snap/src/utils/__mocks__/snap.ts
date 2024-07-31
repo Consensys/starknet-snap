364523f501e15f609f4acfea61f54cbed4cc899f
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-07-31 19:29:31 +0800

    chore: add mock for snap helper

---
+++ packages/starknet-snap/src/utils/__mocks__/snap.ts
@@ -0,0 +1,9 @@
+export const getProvider = jest.fn();
+
+export const getBip44Deriver = jest.fn();
+
+export const confirmDialog = jest.fn();
+
+export const getStateData = jest.fn();
+
+export const setStateData = jest.fn();
