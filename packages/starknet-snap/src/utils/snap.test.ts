fd235444952de446d8d6ea108066ddf623fa43a6
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-07-31 17:27:13 +0800

    chore: add snap utils (#306)

---
+++ packages/starknet-snap/src/utils/snap.test.ts
@@ -0,0 +1,101 @@
+import type { Component } from '@metamask/snaps-sdk';
+import { heading, panel, text, divider, row } from '@metamask/snaps-sdk';
+
+import * as snapUtil from './snap';
+
+jest.mock('@metamask/key-tree', () => ({
+  getBIP44AddressKeyDeriver: jest.fn(),
+}));
+
+describe('getBip44Deriver', () => {
+  it('gets bip44 deriver', async () => {
+    const spy = jest.spyOn(snapUtil.getProvider(), 'request');
+
+    await snapUtil.getBip44Deriver();
+
+    expect(spy).toHaveBeenCalledWith({
+      method: 'snap_getBip44Entropy',
+      params: {
+        coinType: 9004,
+      },
+    });
+  });
+});
+
+describe('confirmDialog', () => {
+  it('calls snap_dialog', async () => {
+    const spy = jest.spyOn(snapUtil.getProvider(), 'request');
+    const components: Component[] = [
+      heading('header'),
+      text('subHeader'),
+      divider(),
+      row('Label1', text('Value1')),
+      text('**Label2**:'),
+      row('SubLabel1', text('SubValue1')),
+    ];
+
+    await snapUtil.confirmDialog(components);
+
+    expect(spy).toHaveBeenCalledWith({
+      method: 'snap_dialog',
+      params: {
+        type: 'confirmation',
+        content: panel(components),
+      },
+    });
+  });
+});
+
+describe('getStateData', () => {
+  it('gets state data', async () => {
+    const spy = jest.spyOn(snapUtil.getProvider(), 'request');
+    const testcase = {
+      state: {
+        transaction: [
+          {
+            txHash: 'hash',
+            chainId: 'chainId',
+          },
+        ],
+      },
+    };
+
+    spy.mockResolvedValue(testcase.state);
+    const result = await snapUtil.getStateData();
+
+    expect(spy).toHaveBeenCalledWith({
+      method: 'snap_manageState',
+      params: {
+        operation: 'get',
+      },
+    });
+
+    expect(result).toStrictEqual(testcase.state);
+  });
+});
+
+describe('setStateData', () => {
+  it('sets state data', async () => {
+    const spy = jest.spyOn(snapUtil.getProvider(), 'request');
+    const testcase = {
+      state: {
+        transaction: [
+          {
+            txHash: 'hash',
+            chainId: 'chainId',
+          },
+        ],
+      },
+    };
+
+    await snapUtil.setStateData(testcase.state);
+
+    expect(spy).toHaveBeenCalledWith({
+      method: 'snap_manageState',
+      params: {
+        operation: 'update',
+        newState: testcase.state,
+      },
+    });
+  });
+});
