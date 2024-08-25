d3570e1a2d11a40c39538fbe89bc1b85e34b1637
Florin Dzeladini <florin.dzeladini@consensys.net>
2024-08-26 01:15:18 +0200

    chore: lint + prettier

--- packages/wallet-ui/src/services/useStarkNetSnap.ts
+++ packages/wallet-ui/src/services/useStarkNetSnap.ts
@@ -32,7 +32,12 @@ import { ethers } from 'ethers';
 import { getAssetPriceUSD } from './coinGecko';
 import semver from 'semver/preload';
 import { setActiveNetwork } from 'slices/networkSlice';
-import { constants, Invocations, TransactionType, UniversalDetails } from 'starknet';
+import {
+  constants,
+  Invocations,
+  TransactionType,
+  UniversalDetails,
+} from 'starknet';
 
 export const useStarkNetSnap = () => {
   const dispatch = useAppDispatch();
