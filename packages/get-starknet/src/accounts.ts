d063aaa85cd6ecd649d91d49939cf453c7fb7b94
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-10-21 10:33:40 +0800

    chore: update get-starknet account obj

--- packages/get-starknet/src/accounts.ts
+++ packages/get-starknet/src/accounts.ts
@@ -36,14 +36,24 @@ export class MetaMaskAccount extends Account {
 
   async execute(
     calls: AllowArray<Call>,
+    // ABIs is deprecated and will be removed in the future
     abisOrTransactionsDetail?: Abi[] | InvocationsDetails,
     details?: InvocationsDetails,
   ): Promise<InvokeFunctionResponse> {
+    // if abisOrTransactionsDetail is an array, we assume it's an array of ABIs
+    // otherwise, we assume it's an InvocationsDetails object
+    if (Array.isArray(abisOrTransactionsDetail)) {
+      return this.#snap.execute({
+        address: this.#address,
+        calls,
+        details,
+        abis: abisOrTransactionsDetail,
+      });
+    }
     return this.#snap.execute({
       address: this.#address,
       calls,
-      details: details ?? (abisOrTransactionsDetail as unknown as InvocationsDetails),
-      abis: details ? (abisOrTransactionsDetail as Abi[]) : undefined,
+      details: abisOrTransactionsDetail as unknown as InvocationsDetails,
     });
   }
 
