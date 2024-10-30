3a9385f5a5a9194685b056d8ec87b3953996f510
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-10-30 16:45:58 +0800

    chore: force to add cors header

--- terraform/live/functions/headers.js
+++ terraform/live/functions/headers.js
@@ -2,7 +2,20 @@ function handler(event) {
     var response = event.response;
     var headers = response.headers;
 
-    headers['x-frame-options'] = {value: 'SAMEORIGIN'}; 
+    try {
+        var request = event.request;
+        // if the request is for the get-starknet, add header to allow cross-origin requests
+        if (request.uri.includes("starknet/get-starknet/v1")) { 
+            headers["access-control-allow-origin"] = { value: "*" };
+            headers["access-control-allow-methods"] = { value: "GET, HEAD" };
+        }
+    } catch (e) {
+        // output errors to the console
+        console.log(e);
+    }
+
     
+    headers['x-frame-options'] = {value: 'SAMEORIGIN'}; 
+
     return response;
 }
