9f43a228e844ab200984a0b5a1f8ff7bb0d8288d
khanti42 <florin.dzeladini@consensys.net>
2024-11-22 09:12:41 +0100

    fix: wallet-ui asset icon management (#428)
    * fix: wallet-ui asset icon management
    
    * chore: fix comments

--- packages/wallet-ui/src/assets/types/assets.ts
+++ packages/wallet-ui/src/assets/types/assets.ts
@@ -2,19 +2,18 @@ import ethIcon from '../images/eth-icon.svg';
 import starknetIcon from '../images/starknet-icon.svg';
 import daiIcon from '../images/dai-icon.svg';
 import aaveIcon from '../images/aave-icon.svg';
+import usdcIcon from '../images/usdc-icon.svg';
+import usdtIcon from '../images/usdt-icon.svg';
 
-export const assetIcons = [ethIcon, daiIcon, aaveIcon];
-export enum AssetIconsIndex {
-  Ether = 0,
-  DAI = 1,
-  AAVE = 2,
-}
+export const assetIcons: Record<string, string> = {
+  ETH: ethIcon,
+  DAI: daiIcon,
+  AAVE: aaveIcon,
+  USDC: usdcIcon,
+  USDT: usdtIcon,
+  STRK: starknetIcon, // Default to starknetIcon for STRK
+};
 
-export const getAssetIcon = (assetName: string): string => {
-  let iconIndex = -1;
-  if (Object.keys(AssetIconsIndex).indexOf(assetName) !== -1) {
-    iconIndex = AssetIconsIndex[assetName as keyof typeof AssetIconsIndex];
-  }
-
-  return assetName ? assetIcons[iconIndex] : starknetIcon;
+export const getAssetIcon = (assetSymbol: string): string => {
+  return assetIcons[assetSymbol] || starknetIcon; // Use starknetIcon as fallback
 };
