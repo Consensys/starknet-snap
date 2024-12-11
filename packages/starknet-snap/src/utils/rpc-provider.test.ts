a94bb1decd57c2e03e8bae691ca2ed930e44359e
Florin Dzeladini <florin.dzeladini@consensys.net>
2024-12-11 10:10:34 +0100

    chore: fix comments

--- packages/starknet-snap/src/utils/rpc-provider.test.ts
+++ packages/starknet-snap/src/utils/rpc-provider.test.ts
@@ -1,23 +1,25 @@
 import { constants } from 'starknet';
 
+import { Config } from '../config';
 import { getRPCUrl } from './rpc-provider';
 
 describe('getRPCUrl', () => {
+  Config.rpcApiKey = 'test';
   it('returns Mainnet RPC URL if chain id is Mainnet', () => {
     expect(getRPCUrl(constants.StarknetChainId.SN_MAIN)).toBe(
-      'https://starknet-mainnet.infura.io/v3/',
+      `https://starknet-mainnet.infura.io/v3/${Config.rpcApiKey}`,
     );
   });
 
   it('returns Sepolia RPC URL if chain id is not either Mainnet or Sepolia', () => {
     expect(getRPCUrl('0x534e5f474f45524c49')).toBe(
-      'https://starknet-sepolia.infura.io/v3/',
+      `https://starknet-sepolia.infura.io/v3/${Config.rpcApiKey}`,
     );
   });
 
   it('returns Sepolia RPC URL if chain id is Sepolia', () => {
     expect(getRPCUrl(constants.StarknetChainId.SN_SEPOLIA)).toBe(
-      'https://starknet-sepolia.infura.io/v3/',
+      `https://starknet-sepolia.infura.io/v3/${Config.rpcApiKey}`,
     );
   });
 });
--- packages/starknet-snap/src/utils/rpc-provider.ts
+++ packages/starknet-snap/src/utils/rpc-provider.ts
@@ -3,15 +3,16 @@ import { constants } from 'starknet';
 import { Config } from '../config';
 
 /**
+ * Gets the rpc URL for a given Chain ID.
  *
- * @param chainId
+ * @param chainId - The Chain ID.
  */
 export function getRPCUrl(chainId: string) {
   switch (chainId) {
     case constants.StarknetChainId.SN_MAIN:
-      return Config.rpcEndpoint[constants.StarknetChainId.SN_MAIN];
+      return `https://starknet-mainnet.infura.io/v3/${Config.rpcApiKey}`;
     default:
     case constants.StarknetChainId.SN_SEPOLIA:
-      return Config.rpcEndpoint[constants.StarknetChainId.SN_SEPOLIA];
+      return `https://starknet-sepolia.infura.io/v3/${Config.rpcApiKey}`;
   }
 }
