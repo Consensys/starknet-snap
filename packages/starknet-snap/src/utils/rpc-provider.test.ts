6eb08bcde49ddc477b0270b1ed36ef4b33c25023
Florin Dzeladini <florin.dzeladini@consensys.net>
2024-12-16 17:40:59 +0100

    chore: handle comment

--- packages/starknet-snap/src/utils/rpc-provider.test.ts
+++ packages/starknet-snap/src/utils/rpc-provider.test.ts
@@ -4,7 +4,12 @@ import { Config } from '../config';
 import { getRPCUrl } from './rpc-provider';
 
 describe('getRPCUrl', () => {
-  Config.rpcApiKey = 'test';
+  beforeEach(function () {
+    Config.rpcApiKey = 'API_KEY';
+  });
+  afterEach(function () {
+    Config.rpcApiKey = '';
+  });
   it('returns Mainnet RPC URL if chain id is Mainnet', () => {
     expect(getRPCUrl(constants.StarknetChainId.SN_MAIN)).toBe(
       `https://starknet-mainnet.infura.io/v3/${Config.rpcApiKey}`,
