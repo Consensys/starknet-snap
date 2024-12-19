eb6c68876659df171a9c3d9421be1ecf4273d2ee
khanti42 <florin.dzeladini@consensys.net>
2024-12-19 10:18:29 +0100

    feat: replace RPC provider from Alchemy to Infura DIN (#451)
    * feat: replace alchemy by infura din
    
    * chore: change secrets from VOYAGER to DIN for rpc calls
    
    * chore: lint+fix
    
    * chore: .env.example
    
    * chore: test import fix
    
    * chore: reduced coverage on old test to 50%
    
    * chore: handle comments
    
    * chore: fix comments
    
    * chore: handle comment
    
    * chore: change ALCHEMY to DIN in CI/CD
    
    * chore: fix comments

---
+++ packages/starknet-snap/src/utils/rpc-provider.ts
@@ -0,0 +1,19 @@
+import { constants } from 'starknet';
+
+import { Config } from '../config';
+
+/**
+ * Gets the rpc URL for a given Chain ID.
+ *
+ * @param chainId - The Chain ID.
+ * @returns The RPC node endpoint of the corresponding chain.
+ */
+export function getRPCUrl(chainId: string) {
+  switch (chainId) {
+    case constants.StarknetChainId.SN_MAIN:
+      return `https://starknet-mainnet.infura.io/v3/${Config.rpcApiKey}`;
+    default:
+    case constants.StarknetChainId.SN_SEPOLIA:
+      return `https://starknet-sepolia.infura.io/v3/${Config.rpcApiKey}`;
+  }
+}
