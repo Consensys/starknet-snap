687462260534d97f0e738cfa214026a8380c8705
Florin Dzeladini <florin.dzeladini@consensys.net>
2025-01-23 21:05:20 +0100

    chore: added translation

--- packages/wallet-ui/src/components/ui/organism/SideBar/SideBar.view.tsx
+++ packages/wallet-ui/src/components/ui/organism/SideBar/SideBar.view.tsx
@@ -37,10 +37,11 @@ export const SideBarView = ({ address }: Props) => {
   const [accountDetailsOpen, setAccountDetailsOpen] = useState(false);
   const wallet = useAppSelector((state) => state.wallet);
   const [addTokenOpen, setAddTokenOpen] = useState(false);
-  const { getStarkName } = useStarkNetSnap();
+  const { getStarkName, getTranslator } = useStarkNetSnap();
   const [starkName, setStarkName] = useState<string | undefined>(undefined);
 
   const ref = useRef<HTMLDivElement>();
+  const translate = getTranslator();
 
   useEffect(() => {
     if (ref.current) {
@@ -67,72 +68,74 @@ export const SideBarView = ({ address }: Props) => {
   }, [address, chainId]);
 
   return (
-    <Wrapper>
-      <PopInStyled
-        isOpen={accountDetailsOpen}
-        setIsOpen={setAccountDetailsOpen}
-      >
-        <AccountDetailsModal address={address} />
-      </PopInStyled>
-      <PopIn
-        isOpen={infoModalOpen}
-        setIsOpen={setInfoModalOpen}
-        showClose={false}
-      >
-        <ConnectInfoModal
-          onButtonClick={() => setInfoModalOpen(false)}
-          address={address}
-        />
-      </PopIn>
-      <AccountDetails
-        arrowVisible={false}
-        closeTrigger="click"
-        offSet={[60, 0]}
-        content={
-          <AccountDetailsContent>
-            <AccountDetailButton
-              backgroundTransparent
-              iconLeft="qrcode"
-              onClick={() => setAccountDetailsOpen(true)}
-            >
-              Account details
-            </AccountDetailButton>
-            <AccountDetailButton
-              backgroundTransparent
-              iconLeft="external-link"
-              onClick={() => openExplorerTab(address, 'contract', chainId)}
-            >
-              View on explorer
-            </AccountDetailButton>
-          </AccountDetailsContent>
-        }
-      >
-        <AccountImageStyled address={address} connected={wallet.connected} />
-      </AccountDetails>
+    translate && (
+      <Wrapper>
+        <PopInStyled
+          isOpen={accountDetailsOpen}
+          setIsOpen={setAccountDetailsOpen}
+        >
+          <AccountDetailsModal address={address} />
+        </PopInStyled>
+        <PopIn
+          isOpen={infoModalOpen}
+          setIsOpen={setInfoModalOpen}
+          showClose={false}
+        >
+          <ConnectInfoModal
+            onButtonClick={() => setInfoModalOpen(false)}
+            address={address}
+          />
+        </PopIn>
+        <AccountDetails
+          arrowVisible={false}
+          closeTrigger="click"
+          offSet={[60, 0]}
+          content={
+            <AccountDetailsContent>
+              <AccountDetailButton
+                backgroundTransparent
+                iconLeft="qrcode"
+                onClick={() => setAccountDetailsOpen(true)}
+              >
+                {translate('accountDetails')}
+              </AccountDetailButton>
+              <AccountDetailButton
+                backgroundTransparent
+                iconLeft="external-link"
+                onClick={() => openExplorerTab(address, 'contract', chainId)}
+              >
+                {translate('viewOnExplorer')}
+              </AccountDetailButton>
+            </AccountDetailsContent>
+          }
+        >
+          <AccountImageStyled address={address} connected={wallet.connected} />
+        </AccountDetails>
 
-      <AccountLabel>My account</AccountLabel>
-      <RowDiv>
-        <InfoIcon onClick={() => setInfoModalOpen(true)}>i</InfoIcon>
-        <AccountAddress address={address} starkName={starkName} />
-      </RowDiv>
-      <DivList ref={ref as any}>
-        <AssetsList />
-      </DivList>
-      <AddTokenButton
-        customIconLeft={
-          <RoundedIcon>
-            <FontAwesomeIcon icon={['fas', 'plus']} />
-          </RoundedIcon>
-        }
-        backgroundTransparent
-        shadowVisible={listOverflow}
-        onClick={() => setAddTokenOpen(true)}
-      >
-        ADD TOKEN
-      </AddTokenButton>
-      <PopIn isOpen={addTokenOpen} setIsOpen={setAddTokenOpen}>
-        <AddTokenModal closeModal={() => setAddTokenOpen(false)} />
-      </PopIn>
-    </Wrapper>
+        <AccountLabel>{translate('myAccount')}</AccountLabel>
+        <RowDiv>
+          <InfoIcon onClick={() => setInfoModalOpen(true)}>i</InfoIcon>
+          <AccountAddress address={address} starkName={starkName} />
+        </RowDiv>
+        <DivList ref={ref as any}>
+          <AssetsList />
+        </DivList>
+        <AddTokenButton
+          customIconLeft={
+            <RoundedIcon>
+              <FontAwesomeIcon icon={['fas', 'plus']} />
+            </RoundedIcon>
+          }
+          backgroundTransparent
+          shadowVisible={listOverflow}
+          onClick={() => setAddTokenOpen(true)}
+        >
+          {translate('addTokenCapital')}
+        </AddTokenButton>
+        <PopIn isOpen={addTokenOpen} setIsOpen={setAddTokenOpen}>
+          <AddTokenModal closeModal={() => setAddTokenOpen(false)} />
+        </PopIn>
+      </Wrapper>
+    )
   );
 };
