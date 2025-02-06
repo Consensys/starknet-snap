31ec54c458ed36687dd185e7270f0f85bb5d1387
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2025-02-06 11:06:28 +0800

    feat: watch the SNAP current account

--- packages/get-starknet/src/wallet.test.ts
+++ packages/get-starknet/src/wallet.test.ts
@@ -1,12 +1,39 @@
 import { Mutex } from 'async-mutex';
+import type { WalletEventHandlers } from 'get-starknet-core';
 import { Provider } from 'starknet';
 
-import { SepoliaNetwork, mockWalletInit, createWallet } from './__tests__/helper';
+import {
+  SepoliaNetwork,
+  mockWalletInit,
+  createWallet,
+  generateAccount,
+  MainnetNetwork,
+  MockMetaMaskSnapWallet,
+} from './__tests__/helper';
 import { MetaMaskAccount } from './accounts';
 import { WalletSupportedSpecs } from './rpcs';
-import type { AccContract, Network } from './type';
+import type { Network } from './type';
 
 describe('MetaMaskSnapWallet', () => {
+  const setupEventTest = async (eventName: keyof WalletEventHandlers) => {
+    const handlers = [jest.fn(), jest.fn()];
+    const wallet = createWallet();
+
+    for (const handler of handlers) {
+      wallet.on(eventName, handler);
+    }
+
+    // Having a delay to make sure the polling is done
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    // Due to polling operation is a endless loop, we need to stop it manually,
+    for (const handler of handlers) {
+      wallet.off(eventName, handler);
+    }
+
+    return { handlers, wallet };
+  };
+
   describe('enable', () => {
     it('returns an account address', async () => {
       const expectedAccountAddress = '0x04882a372da3dfe1c53170ad75893832469bf87b62b13e84662565c4a88f25cd'; // in hex
@@ -18,15 +45,6 @@ describe('MetaMaskSnapWallet', () => {
       const [address] = await wallet.enable();
 
       expect(address).toStrictEqual(expectedAccountAddress);
-    });
-
-    it('throws `Unable to recover accounts` error if the account address not return from the Snap', async () => {
-      const { recoverDefaultAccountSpy } = mockWalletInit({});
-      recoverDefaultAccountSpy.mockResolvedValue({} as unknown as AccContract);
-
-      const wallet = createWallet();
-
-      await expect(wallet.enable()).rejects.toThrow('Unable to recover accounts');
     });
   });
 
@@ -137,18 +155,94 @@ describe('MetaMaskSnapWallet', () => {
   });
 
   describe('on', () => {
-    it('does nothing and not throw any error', async () => {
+    it('adds an event handler and starts polling if not already started', async () => {
+      mockWalletInit({});
+      const addHandlerSpy = jest.spyOn(Set.prototype, 'add');
+      const startPollingSpy = jest.spyOn(MockMetaMaskSnapWallet.prototype, 'startPolling');
+
+      const { handlers } = await setupEventTest('accountsChanged');
+
+      expect(addHandlerSpy).toHaveBeenCalledTimes(handlers.length);
+      for (let i = 0; i < handlers.length; i++) {
+        expect(addHandlerSpy).toHaveBeenNthCalledWith(i + 1, handlers[i]);
+      }
+      expect(startPollingSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('throws an error for unsupported events', () => {
       const wallet = createWallet();
 
-      expect(() => wallet.on('accountsChanged', jest.fn())).not.toThrow();
+      expect(() => wallet.on('unsupportedEvent' as any, jest.fn())).toThrow('Unsupported event: unsupportedEvent');
     });
   });
 
   describe('off', () => {
-    it('does nothing and not throw any error', async () => {
-      const wallet = createWallet();
+    it('removes an event handler and stops polling if no handlers remain', async () => {
+      mockWalletInit({});
+      const deleteHandlerSpy = jest.spyOn(Set.prototype, 'delete');
+      const stopPollingSpy = jest.spyOn(MockMetaMaskSnapWallet.prototype, 'stopPolling');
 
-      expect(() => wallet.off('accountsChanged', jest.fn())).not.toThrow();
+      const { handlers } = await setupEventTest('accountsChanged');
+
+      expect(deleteHandlerSpy).toHaveBeenCalledTimes(handlers.length);
+      for (let i = 0; i < handlers.length; i++) {
+        expect(deleteHandlerSpy).toHaveBeenNthCalledWith(i + 1, handlers[i]);
+      }
+      expect(stopPollingSpy).toHaveBeenCalledTimes(1);
     });
+
+    it('throws an error for unsupported events', () => {
+      const wallet = createWallet();
+      expect(() => wallet.off('unsupportedEvent' as any, jest.fn())).toThrow('Unsupported event: unsupportedEvent');
+    });
+  });
+
+  describe('event handling', () => {
+    it('dispatchs a `accountsChanged` event', async () => {
+      const { address: initialAddress } = generateAccount({ address: '0xInitialAddress' });
+      const { address: newAddress } = generateAccount({ address: '0xNewAddress' });
+
+      // The code simulates a scenario where the initial address is the default account address.
+      // Later, the address is changed to a new address and remains unchanged.
+      // - `mockResolvedValueOnce` sets the initial address as the default account address.
+      // - `mockResolvedValue` from `mockWalletInit` sets the new address as the new default.
+      const { getCurrentAccountSpy } = mockWalletInit({ address: newAddress });
+      getCurrentAccountSpy.mockResolvedValueOnce(generateAccount({ address: initialAddress }));
+
+      const { handlers } = await setupEventTest('accountsChanged');
+
+      for (const handler of handlers) {
+        expect(handler).toHaveBeenCalledWith([newAddress], undefined);
+      }
+    });
+
+    it('dispatchs a `networkChanged` event', async () => {
+      // The code simulates a scenario where the MainnetNetwork is the default network.
+      // Later, the network is changed to SepoliaNetwork and remains unchanged.
+      // - `mockResolvedValueOnce` sets the MainnetNetwork as the default network.
+      // - `mockResolvedValue` from `mockWalletInit` sets the SepoliaNetwork as the new network.
+      const { address } = generateAccount({});
+      const { getCurrentNetworkSpy } = mockWalletInit({ currentNetwork: SepoliaNetwork, address });
+      getCurrentNetworkSpy.mockResolvedValueOnce(MainnetNetwork);
+
+      const { handlers } = await setupEventTest('networkChanged');
+
+      for (const handler of handlers) {
+        expect(handler).toHaveBeenCalledWith(SepoliaNetwork.chainId, [address]);
+      }
+    });
+
+    it.each(['accountsChanged', 'networkChanged'])(
+      'does not dispatchs a %s event if the wallet object is not initialized yet',
+      async (event: keyof WalletEventHandlers) => {
+        mockWalletInit({});
+
+        const { handlers } = await setupEventTest(event);
+
+        for (const handler of handlers) {
+          expect(handler).toHaveBeenCalledTimes(0);
+        }
+      },
+    );
   });
 });
