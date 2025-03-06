9369cd2fb35ad765337a97b67ebd6e2e7e18200e
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2025-03-06 17:12:45 +0800

    chore(snap): remove hide show RPC

--- packages/starknet-snap/src/wallet/account/account.ts
+++ packages/starknet-snap/src/wallet/account/account.ts
@@ -6,7 +6,6 @@ import type { CairoAccountContract } from './contract';
 
 export const DefaultAccountMetaData: AccountMetaData = {
   accountName: getDefaultAccountName(0),
-  visibility: true,
 };
 
 /**
@@ -64,12 +63,8 @@ export class Account {
 
   #jsonDataToMetaData(jsonData?: Partial<AccContract>): void {
     if (!jsonData) {
-      this.metadata.visibility = true;
       this.metadata.accountName = getDefaultAccountName(this.hdIndex);
       return;
-    }
-    if (jsonData.visibility !== undefined) {
-      this.metadata.visibility = jsonData.visibility;
     }
     if (jsonData.accountName === undefined) {
       this.metadata.accountName = getDefaultAccountName(this.hdIndex);
@@ -99,7 +94,6 @@ export class Account {
       cairoVersion: this.cairoVersion,
       deployRequired,
       upgradeRequired,
-      visibility: this.metadata.visibility,
       accountName: this.metadata.accountName,
     };
   }
